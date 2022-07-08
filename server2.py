002c146f9fde32710182bad8ae360e1afec98532
Kevin Xia <kevinxia2015@gmail.com>
2022-07-07 20:33:45 -0400

    My portfolio website

---
+++ server2.py
@@ -0,0 +1,44 @@
+from flask import Flask, render_template, url_for, request, redirect
+import csv
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/<string:page_name>')
+def page(page_name):
+    return render_template(page_name)
+
+def write_file(data):
+    with open('database.txt', mode = 'a') as database2:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database2.write(f'\n{email}, {subject}, {message}')
+
+
+def write_csv(data):
+    with open('database.csv', newline='', mode = 'a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting = csv.QUOTE_MINIMAL)
+        csv_writer.writerow([email,subject,message])
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+    if request.method == 'POST':
+        try:
+            data = request.form.to_dict()
+            write_csv(data)
+        except:
+            return 'Failed to save to database'
+        return redirect('./thankyou.html')
+    else:
+        return 'Something went wrong, try again'
+
+# @app.route('/favicon.ico')
+# def icon():
+#     return 'Something.'
